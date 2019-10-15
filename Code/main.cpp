7f7a3626b605b8d1e04784fb078f905268e2500d
Matheus Ibrahim <matheusibrahimfm@gmail.com>
2019-10-15 14:21:36 +0000

    Organizing code

--- Code/main.cpp
+++ Code/main.cpp
@@ -13,53 +13,61 @@ using std::endl;
 
 
 int main(int argc, char** argv){
-    string fileName = argv[1];
+    if(argc > 1){ //Checks if the file name was passed as a program parameter
 
-    ifstream input(fileName);
+        string fileName = argv[1];
+    
+        ifstream input(fileName);
 
-    int moneyToSpend, ammountIslands;
-    int *costs, *points;
+        int moneyToSpend, ammountIslands;
+        int *costs, *points;
 
 
-    if(input.is_open()){
-        input >> moneyToSpend >> ammountIslands;
+        if(input.is_open()){
+            input >> moneyToSpend >> ammountIslands;
 
-        costs = new int[ammountIslands];
-        points = new int[ammountIslands];
+            costs = new int[ammountIslands];
+            points = new int[ammountIslands];
+            
+            for(int i = 0; i < ammountIslands; i++){
+                input >> costs[i] >> points[i];
+            }
+
+            Island islands[ammountIslands];
+            for(int i = 0; i < ammountIslands; i++){
+                islands[i] = Island(costs[i], points[i]);
+            }
+
+            Trip t(moneyToSpend, ammountIslands, islands);
+
+
+            auto startWithRep = high_resolution_clock::now();
+
+            t.MaxPointsRepeatingIslands();
+
+            auto endWithRep = high_resolution_clock::now();
+            auto durationWithRep = duration_cast<microseconds>(endWithRep - startWithRep); 
+
+            cout << "Time taken by function MaxPointsRepeatingIslands: "
+                << durationWithRep.count() << " microseconds" << endl;
+
+            auto start = high_resolution_clock::now();
         
-        for(int i = 0; i < ammountIslands; i++){
-            input >> costs[i] >> points[i];
+            t.MaxPointsWithoutRepeating();
+            
+            auto end = high_resolution_clock::now();
+            auto duration = duration_cast<microseconds>(end - start); 
+
+            cout << "Time taken by function MaxPointsWithoutRepeating: "
+                << duration.count() << " microseconds" << endl;
         }
-
-
-        Island islands[ammountIslands];
-        for(int i = 0; i < ammountIslands; i++){
-            islands[i] = Island(costs[i], points[i]);
+        else{
+            cout << "Couldn't open file " << fileName << 
+                ", please check if the file name is correct and if it actually exists" << endl;;
         }
-
-        Trip t(moneyToSpend, ammountIslands, islands);
-        auto startRep = high_resolution_clock::now();
-        t.MaxPointsRepeatingIslands();
-        auto endRep = high_resolution_clock::now();
-
-        auto duration = duration_cast<microseconds>(endRep - startRep); 
-
-        cout << "Time taken by function: "
-            << duration.count() << " microseconds" << endl;
-
-        auto start = high_resolution_clock::now();
-       
-        t.MaxPointsWithoutRepeating();
-        auto end = high_resolution_clock::now();
-
-        auto duration = duration_cast<microseconds>(end - start); 
-
-        cout << "Time taken by function: "
-            << duration.count() << " microseconds" << endl;
     }
     else{
-        cout << "Erro ao abrir o arquivo " << fileName << endl;;
+        cout << "Please pass a file name as a parameter to the program" << endl;
     }
-
      
 }
